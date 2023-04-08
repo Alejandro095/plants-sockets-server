1bfa8edcca3b1b9f8f2e1573d3060213250b6a8f
Alejandro Tovar <javi.monsi2009@hotmail.com>
2023-04-07 22:14:49 -0600

    Se agregaron mejores logs.

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ wss.on('connection', function connection(ws) {
 function handlerEventMessage(payload, ws) {
   const [origin, key, data] = payload.split('~');
 
+  console.log('NEW RESQUEST: ', {origin, key, data});
+
   switch (key) {
     case 'set-data': // origin -> microcontroller, example: 'microcontroller~set-data~10|20|30|40'
       const [luz, humedad, tempAire, humedadAire] = data.split('|');
@@ -29,17 +31,23 @@ function handlerEventMessage(payload, ws) {
       storeData.humedad = humedad;
       storeData.tempAire = tempAire;
       storeData.humedadAire = humedadAire;
+
+      const resp = `client-web~set-data~${storeData.luz}|${storeData.humedad}|${storeData.tempAire}|${storeData.humedadAire}`
+
+      console.log('RESPONSE_TO_ALL_CLIENTS: ', resp);
+
       // Enviar la informacion a todos los clientes web
-      wss.clients.forEach(client => client.send(
-        `client-web~set-data~${storeData.luz}|${storeData.humedad}|${storeData.tempAire}|${storeData.humedadAire}`
-      ))
+      wss.clients.forEach(client => client.send(resp))
       break;
     case 'set-action': // origin -> client-web, example: 'client-web~set-action~luz'
       const action = data;
+
+      const response = `microcontroller~set-action~${action}`;
+
+      console.log('RESPONSE_TO_ALL_CLIENTS: ', response);
+
       // Enviar la accion a microcontroller
-      wss.clients.forEach(client => client.send(
-        `microcontroller~set-action~${action}`
-      ));
+      wss.clients.forEach(client => client.send(response));
       break;
   }
 }
